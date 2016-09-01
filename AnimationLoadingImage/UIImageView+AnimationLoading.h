8e375ddc121d50bbe110ea9eba75372a9d8df2a4
Ali <604999439@qq.com>
2016-09-01 17:15:05 +0800

    添加注释

--- AnimationLoadingImage/UIImageView+AnimationLoading.h
+++ AnimationLoadingImage/UIImageView+AnimationLoading.h
@@ -9,7 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface UIImageView (AnimationLoading)
-
+/**
+ *  @author Ali
+ *
+ *  下载图片 并且动画显示
+ *
+ *  @param strUrl 图片地址(字符串哦)
+ *  @param pImage 占位图片
+ */
 -(void)setImageWithAnimationOfUrl:(NSString *)strUrl placeHolder:(UIImage *)pImage;
 
 @end
