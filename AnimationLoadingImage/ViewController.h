f4357847563040c27bbd650f65703f0c1ffede26
Ali <604999439@qq.com>
2016-09-01 10:36:34 +0800

    Initial Commit

---
+++ AnimationLoadingImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationLoadingImage
+//
+//  Created by Almas on 16/9/1.
+//  Copyright © 2016年 Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationLoadingImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AnimationLoadingImage
+//
+//  Created by Almas on 16/9/1.
+//  Copyright © 2016年 Ali. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
